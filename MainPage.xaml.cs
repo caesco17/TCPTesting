7030902d84dbd3da857c1d9e4e26db30cf894512
caesco17 <caescobar17@gmail.com>
2024-04-02 17:16:49 -0600

    Update to test on macos xml file

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Text;
 using TCPTesting.Services;
+using System.IO;
 
 namespace TCPTesting
 {
@@ -10,20 +11,22 @@ namespace TCPTesting
         private ITCPService _TCPService;
         private Socket? _socket;
         private bool isOpen = false;
+        private string? _resultfile = string.Empty;
+
         public MainPage(ITCPService service)
         {
             InitializeComponent();
             _TCPService = service;
         }
 
-        private async void OpenSocket_Clicked(object sender, EventArgs e)
+        private void OpenSocket_Clicked(object sender, EventArgs e)
         {
             string formipAddress = IpEntry.Text.Trim();
             errorLabel.Text = string.Empty;
 
             var data = formipAddress.Split(":").ToList();
 
-            if(data.Count != 2)
+            if (data.Count != 2)
             {
                 errorLabel.Text = "Invalid IP.";
                 return;
@@ -42,8 +45,6 @@ namespace TCPTesting
                 else
                 {
                     _socket = _TCPService.GetSocket(baseIp, basePort);
-                    await _socket!.ConnectAsync(_TCPService.getEndPoint()!);
-                    isOpen = true;
                 }
 
             }
@@ -56,25 +57,61 @@ namespace TCPTesting
 
         private async void SendMessage_Clicked(object sender, EventArgs e)
         {
-            string formMessage = MessageEntry.Text.Trim();
+            await _socket!.ConnectAsync(_TCPService.getEndPoint()!);
+            isOpen = true;
+
+            await progressBar.ProgressTo(0, 500, Easing.Linear);
+            string formMessage = MessageEntry.Text;
             infoLabel.Text = "";
 
             if (isOpen)
             {
+                if (_resultfile!.Length > 0)
+                {
+                    var messageBytes = Encoding.UTF8.GetBytes(_resultfile);
+                    _ = await _socket!.SendAsync(messageBytes, SocketFlags.None);
+                    infoLabel.Text = $"Socket client sent message: \"{messageBytes}\"";
+                    _resultfile = string.Empty;
+                    await progressBar.ProgressTo(0.75, 500, Easing.Linear);
+                    _socket.Disconnect(true);
+                }
+                else if (formMessage.Length > 0)
+                {
+                    var messageBytes = Encoding.UTF8.GetBytes(formMessage);
+                    _ = await _socket!.SendAsync(messageBytes, SocketFlags.None);
+                    infoLabel.Text = $"Socket client sent message: \"{messageBytes}\"";
+                    await progressBar.ProgressTo(0.75, 500, Easing.Linear);
+                    _socket.Disconnect(true);
+                }
+                else
+                {
+                    _socket.Disconnect(true);
+                    return;
+                }
 
-                var messageBytes = Encoding.UTF8.GetBytes(formMessage);
-                _ = await _socket!.SendAsync(messageBytes, SocketFlags.None);
 
-                infoLabel.Text = $"Socket client sent message: \"{formMessage}\"";
+                //var buffer = new byte[1_024];
+                //var received = await _socket!.ReceiveAsync(buffer, SocketFlags.None);
+                //var response = Encoding.UTF8.GetString(buffer, 0, received);
+                //Thread.Sleep(1000);
 
-
-                var buffer = new byte[1_024];
-                var received = await _socket!.ReceiveAsync(buffer, SocketFlags.None);
-                var response = Encoding.UTF8.GetString(buffer, 0, received);
-                Thread.Sleep(1000);
-
-                receiveLabel.Text = $"Socket client received: \"{response}\"";
+                //receiveLabel.Text = $"Socket client received: \"{response}\"";
+                await progressBar.ProgressTo(1, 500, Easing.Linear);
+                isOpen = false;
             }
+        }
+
+        private async void  Button_Clicked(object sender, EventArgs e)
+        {
+            var files = await FilePicker.Default.PickAsync(new PickOptions
+            {
+                PickerTitle = "Pick file"
+            });
+
+            var _source = await files!.OpenReadAsync();
+
+            StreamReader reader = new StreamReader(_source, System.Text.Encoding.UTF8);
+            _resultfile = reader.ReadToEnd();
         }
     }
 }
